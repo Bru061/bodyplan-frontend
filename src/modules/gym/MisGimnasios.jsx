3f9057a02183fd1c9125e40c71f85383d984207a
Bru061 <brunobarcenas9885@gmail.com>
2026-03-02 09:59:43 -0600

    fix changes actives

--- src/modules/gym/MisGimnasios.jsx
+++ src/modules/gym/MisGimnasios.jsx
@@ -3,18 +3,28 @@ import DashboardLayout from "../../layout/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import api from "../../services/axios";
 import "../../styles/gimnasio.css";
-import { FiPlus, FiMapPin, FiTrash2, FiEdit } from "react-icons/fi";
+import { FiPlus} from "react-icons/fi";
 
 function MisGimnasios(){
 
   const [gimnasios,setGimnasios] = useState([]);
   const [loading,setLoading] = useState(true);
   const navigate = useNavigate();
+  const [changingId,setChangingId] = useState(null);
+  const gimnasiosOrdenados = [...gimnasios].sort((a,b)=>b.activo - a.activo);
 
   const fetchGyms = async () =>{
     try{
-      const res = await api.get("/gym");
-      setGimnasios(res.data.gimnasios || []);
+      const resActivos = await api.get("/gym");
+      const resArchivados = await api.get("/gym/desactivados");
+      
+      const activos = resActivos.data.gimnasios || [];
+      const archivados = resArchivados.data.gimnasios || [];
+
+      const todos = [...activos, ...archivados];
+
+      setGimnasios(todos);
+
     }catch(err){
       console.error("Error cargando gimnasios",err);
     }finally{
@@ -45,6 +55,8 @@ function MisGimnasios(){
         );
         if (!confirm) return;
 
+        setChangingId(gym.id_gimnasio);
+
         if (gym.activo) {
         await api.put(`/gym/down/${gym.id_gimnasio}`);
         } else {
@@ -56,6 +68,8 @@ function MisGimnasios(){
     } catch (err) {
         console.error(err);
         alert("Error cambiando estado");
+    } finally {
+      setChangingId(null);
     }
     };
 
@@ -91,12 +105,23 @@ function MisGimnasios(){
           </div>
         )}
 
+        {gimnasiosOrdenados.map(g => (
+          <div key={g.iid_gimnasio} className="service-profile-card">
+          </div>
+        ))}
+
         {gimnasios.map(g => (
           <div
             key={g.id_gimnasio}
             className="service-profile-card"
             style={{cursor:"pointer"}}
-            onClick={()=>navigate(`/gimnasio/${g.id_gimnasio}`)}
+            onClick={()=>{
+              if(!g.activo){
+                alert("Este gimnasio está archivado. Actívalo para administrarlo.");
+                return;
+              }
+              navigate(`/gimnasio/${g.id_gimnasio}`);
+            }}
           >
 
             {/* portada */}
@@ -111,7 +136,7 @@ function MisGimnasios(){
               <div className="cover-content">
                 <h2 style={{fontSize:"1.4rem"}}>{g.nombre}</h2>
                 <span className={g.activo ? "badge-activo" : "badge-off"}>
-                  {g.activo ? "Activo" : "Archivado"}
+                  {g.activo ? "🟢 Activo" : "🔴 Archivado"}
                 </span>
                 <p>
                   {g.Ubicacion?.municipio}, {g.Ubicacion?.estado}
@@ -119,7 +144,6 @@ function MisGimnasios(){
               </div>
             </div>  
 
-            {/* info rápida */}
             <div style={{padding:20}}>
               <span>{g.descripcion}</span>
             </div>
@@ -127,8 +151,13 @@ function MisGimnasios(){
             <div className="actions-bar">
               <button
                 className="btn btn-primary"
+                disabled={!g.activo}
                 onClick={(e)=>{
-                e.stopPropagation(); // evita abrir gym
+                e.stopPropagation();
+                if(!g.activo){
+                  alert("Acriva el gimnasio para poder administrarlo");
+                  return;
+                }
                 navigate(`/gimnasio/${g.id_gimnasio}`);
                 }}
               >
@@ -136,13 +165,16 @@ function MisGimnasios(){
               </button>
 
               <button
+                  disabled={changingId === g.id_gimnasio}
                   className={g.activo ? "btn btn-danger" : "btn btn-success"}
                   onClick={(e)=>{
                   e.stopPropagation();
                   toggleActivo(g);
                   }}
               >
-                  {g.activo ? "Archivar" : "Activar"}
+                  {changingId === g.id_gimnasio
+                   ? "Procesando..."
+                   : g.activo ? "Archivar" : "Activar"}
               </button>
             </div>
 
