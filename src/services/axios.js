d7350348600ff7316ffcec9c5bb6fadd32f430cb
Bru061 <brunobarcenas9885@gmail.com>
2026-03-27 22:53:07 -0600

    ahora si el bueno (ahora si)

--- src/services/axios.js
+++ src/services/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.PROD
     ? "/api"
-    : "https://bodyplan-api.giize.com/api"
+    : "https://bodyplan-api.giize.com/"
 });
 
 // INTERCEPTOR → agrega token automáticamente
